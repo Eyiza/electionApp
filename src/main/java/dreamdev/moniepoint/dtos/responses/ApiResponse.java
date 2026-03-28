e4f6b48835ed0e65d323c56d6794c4297fa268d2
Precious Michael <precious.michael2002@gmail.com>
2026-03-28 14:45:40 +0100

    add: Candidate response dto

--- src/main/java/dreamdev/moniepoint/dtos/responses/ApiResponse.java
+++ src/main/java/dreamdev/moniepoint/dtos/responses/ApiResponse.java
@@ -1,4 +1,10 @@
 package dreamdev.moniepoint.dtos.responses;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class ApiResponse {
+    private boolean success;
+    private Object data;
 }
--- src/main/java/dreamdev/moniepoint/dtos/responses/CandidateCreationResponse.java
+++ src/main/java/dreamdev/moniepoint/dtos/responses/CandidateCreationResponse.java
@@ -1,4 +1,11 @@
 package dreamdev.moniepoint.dtos.responses;
 
+import lombok.Data;
+
+@Data
 public class CandidateCreationResponse {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String position;
 }
