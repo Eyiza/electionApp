c58e0f87300e426f2d2087ba4a0594d3f59adc41
eyiza <precious.michael2002@gmail.com>
2026-03-28 16:58:08 +0100

    add: Mappers

--- src/main/java/dreamdev/moniepoint/utils/Mapper.java
+++ src/main/java/dreamdev/moniepoint/utils/Mapper.java
@@ -1,4 +1,34 @@
 package dreamdev.moniepoint.utils;
 
+import dreamdev.moniepoint.data.models.Candidate;
+import dreamdev.moniepoint.dtos.requests.CandidateRequest;
+import dreamdev.moniepoint.dtos.responses.CandidateCreationResponse;
+import dreamdev.moniepoint.dtos.responses.CandidateResponse;
+
 public class Mapper {
+    public static Candidate map(CandidateRequest candidateRequest) {
+        Candidate candidate = new Candidate();
+        candidate.setFirstName(candidateRequest.getFirstName());
+        candidate.setLastName(candidateRequest.getLastName());
+        candidate.setPosition(candidateRequest.getPosition());
+        return candidate;
+    }
+
+    public static CandidateCreationResponse mapToCreationResponse(Candidate candidate) {
+        CandidateCreationResponse candidateResponse = new CandidateCreationResponse();
+        candidateResponse.setFirstName(candidate.getFirstName());
+        candidateResponse.setLastName(candidate.getLastName());
+        candidateResponse.setPosition(candidate.getPosition());
+        return candidateResponse;
+    }
+
+    public static CandidateResponse map(Candidate candidate) {
+        CandidateResponse candidateResponse = new CandidateResponse();
+        candidateResponse.setId(candidate.getId());
+        candidateResponse.setFirstName(candidate.getFirstName());
+        candidateResponse.setLastName(candidate.getLastName());
+        candidateResponse.setPosition(candidate.getPosition());
+        candidateResponse.setVoteCount(candidate.getVoteCount());
+        return candidateResponse;
+    }
 }
