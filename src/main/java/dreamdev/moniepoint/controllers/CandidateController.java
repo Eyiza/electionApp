a7b1107557350bc8cff986c00ff0c91b7b13c96f
Precious Michael <precious.michael2002@gmail.com>
2026-03-28 21:18:38 +0100

    add: Create Candidate Controller

--- src/main/java/dreamdev/moniepoint/controllers/CandidateController.java
+++ src/main/java/dreamdev/moniepoint/controllers/CandidateController.java
@@ -1,9 +1,15 @@
 package dreamdev.moniepoint.controllers;
 
 import dreamdev.moniepoint.data.models.Candidate;
+import dreamdev.moniepoint.dtos.requests.CandidateRequest;
+import dreamdev.moniepoint.dtos.responses.ApiResponse;
+import dreamdev.moniepoint.exceptions.ElectionAppException;
 import dreamdev.moniepoint.services.CandidateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +18,11 @@ public class CandidateController {
     private CandidateService candidateService;
 
     @PostMapping("/candidate")
-    public Candidate addCandidate(){
-        return null;
+    public ResponseEntity<?> addCandidate(@RequestBody CandidateRequest candidateRequest){
+        try {
+            return new ResponseEntity<>(new ApiResponse(true, candidateService.createCandidate(candidateRequest)), HttpStatus.CREATED);
+        } catch (ElectionAppException e){
+            return new ResponseEntity<>(new ApiResponse(false, e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
     }
 }
